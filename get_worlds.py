522d6dbb39025cd0a0e8eec0eb503b4df2d79a73
econeil7 <ec.oneil93@gmail.com>
2023-01-07 21:18:53 -0500

    gets all current worlds from the api

---
+++ get_worlds.py
@@ -0,0 +1,22 @@
+import requests
+
+def get_worlds_lst():
+    """
+    This function grabs the worlds from the TibiaData API and returns them as a list.
+
+    Returns:
+        worlds: A list of the worlds.
+    """
+    endpoint = 'https://api.tibiadata.com/v3/worlds'
+    response = requests.get(endpoint)
+    data = response.json()
+
+    worlds = []
+    for world in data['worlds']['regular_worlds']:
+        worlds.append(world['name'])
+    
+    return worlds
+
+if __name__ == '__main__':
+    for world in worlds:
+        print(world)
